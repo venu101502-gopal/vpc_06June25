dad27c88ab2d88ebd40d0c343e4c5a06f72e95af
venu101502-gopal <venuajet101502@gmail.com>
2025-06-06 17:35:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,51 +1,41 @@
 variable "region" {
   type = string
-
 }
 
 variable "vpc_name" {
   type = string
-  
 }
 
 variable "internet_gateway" {
   type = string
-  
 }
 
 variable "public_subnet_name" {
   type = string
-  
 }
 
 variable "private_subnet_name" {
   type = string
-  
 }
 
 variable "cidr_block" {
   type = string
-  
 }
 
 variable "security_ports" {
-  type = string
-  
+  type = list(number)  # Changed to list(number) if it's meant to be a list of ports
 }
 
 variable "security_ports_ingress" {
   type = list(number)
-  
 }
 
 variable "security_ports_egress" {
-  type = string
-  
+  type = string  # Keep as string if it's a CIDR block or a specific value
 }
 
 variable "public_subnet_avz" {
   type = string
-  
 }
 
 variable "private_subnet_avz" {
@@ -54,27 +44,22 @@ variable "private_subnet_avz" {
 
 variable "public_subnet_cidr" {
   type = string
-  
 }
 
 variable "private_subnet_cidr" {
   type = string
-  
 }
 
 variable "author" {
   type = string
-  
 }
 
 variable "when" {
   type = string
-  
 }
 
 variable "route_cidr" {
   type = string
-  
 }
 
 variable "route_table_name" {
@@ -83,10 +68,8 @@ variable "route_table_name" {
 
 variable "security_group" {
   type = string
-  
 }
 
 variable "cidr_blocks" {
   type = string
-  
 }
