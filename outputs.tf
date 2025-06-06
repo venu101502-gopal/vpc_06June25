63c158963801b8ef759571cce8a4b745d810423c
venu101502-gopal <venuajet101502@gmail.com>
2025-06-06 17:17:36 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,49 @@
+output "region" {
+  value = var.region
+  
+}
+
+output "public_subnet_avz" {
+  value = var.public_subnet_avz
+  
+}
+
+output "public_subnet_name" {
+  value = var.public_subnet_name
+  
+}
+
+output "private_subnet_name" {
+  value = var.private_subnet_name
+  
+}
+
+output "author" {
+  value = var.author
+  
+}
+
+output "when" {
+  value = var.when
+  
+}
+
+output "route_table_name" {
+  value = var.route_table_name
+  
+}
+
+output "security_group" {
+  value = var.security_group
+  
+}
+
+output "vpc_name" {
+  value = var.vpc_name
+  
+}
+
+output "internet_gateway" {
+  value = var.internet_gateway
+  
+}
